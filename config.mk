6ccc0065558857608aed1e80137402818dfa8b08
Doggo <fearlessdoggo21@vivaldi.net>
2022-02-24 13:23:23 -0600

    update documentation

--- config.mk
+++ config.mk
@@ -7,6 +7,10 @@ VERSION = 1.0.0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
+RTCONF = not installed
+CLEARENV = not installed
+USERMOD = not installed
+
 GROUP = wheel
 
 WPROFILE = -Wall -Wextra -Wstrict-prototypes -Wmissing-declarations \
